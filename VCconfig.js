0d17efa710ca8b12b10fcad9c78d3dee7e210059
lafish <lafish@lafish.fun>
2022-04-24 12:21:54 +0800

    1

--- VCconfig.js
+++ VCconfig.js
@@ -2,7 +2,7 @@ const Config = {
     dbUrl: `${process.env.DBURL||"mongodb://root:asdfgh@114.132.221.156:27017"}`,
     dbName: `${process.env.DBNAME||"qrhub"}`,
     serviceURL: `${process.env.SERVICEURL||"http://localhost"}`,
-    servicePort: `${process.env.PORT||80}`,
+    servicePort: `${process.env.PORT||8888}`,
     truePort: '',
 }
 module.exports = Config;
